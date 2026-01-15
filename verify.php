4f6d42247044d48322659660269a4da675e7009b
FUNK Records <xltrecordslebal@gmail.com>
2026-01-15 19:40:42 +0530

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,15 +1,31 @@
 <?php
-$data = json_decode(file_get_contents("php://input"), true);
 
+$data = json_decode(file_get_contents("php://input"), true);
 $orderID = $data['orderID'];
 
-// Here you verify order from PayPal API (secure way)
-// After verification:
+// PAYPAL API VERIFY
+$clientId = "ATsTDg8o88cVJJL5YPUCzySw_F_NeVAqCclxW17v8jvxiUoUiQjwck8vM4ZxrxYhuLV0apKT4Vru7uV2";
+$secret = "EG-RN4MWmdEW8wCkj-fEdNNSeccJfNcO4-ys96gdANb7z1EhW1orWfApN1TjCW43o44FWuOCmxUEXdyT";
 
-session_start();
-$_SESSION['pro_user'] = true;
+$ch = curl_init();
 
-echo json_encode([
-  "status" => "success"
-]);
+curl_setopt($ch, CURLOPT_URL, "https://api-m.sandbox.paypal.com/v2/checkout/orders/$orderID");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_USERPWD, $clientId . ":" . $secret);
+
+$result = curl_exec($ch);
+curl_close($ch);
+
+$response = json_decode($result, true);
+
+if($response['status'] == "COMPLETED"){
+
+    session_start();
+    $_SESSION['pro_user'] = true;
+
+    echo json_encode(["status"=>"success"]);
+
+}else{
+    echo json_encode(["status"=>"failed"]);
+}
 ?>
